6df36207b0a53e65439ac98015aeebc9713a13f1
mars800926gh <mars800926wk@gmail.com>
2018-03-31 04:43:18 +0800

    first commit

---
+++ 20180316t2.py
@@ -0,0 +1,20 @@
+﻿import random
+
+rndbuf = []
+rndget = []
+
+print("Here will get 4 random numbers!")
+#rndamnt = raw_input("please enter the amount of random number : ")
+rndamnt= input("Please enter the amount of random number : ")
+rndmax= input("Please enter the maxnum of random number : ")
+rndamnt=int(rndamnt)
+rndmax=int(rndmax)
+
+while rndamnt>0:
+    rndbuf.append(random.randint(0,rndmax))
+    rndamnt-=1
+print("Total random numbers : ",rndbuf)
+
+while len(rndbuf)>4:
+    rndbuf.pop(random.randint(0,len(rndbuf)-1))
+print("Get 4 random numbers : ",rndbuf)
